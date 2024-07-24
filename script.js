2679eb44ed54f90881297d1910c15323dac2ecd3
Daniel Vieira <danielvnso7@gmail.com>
2024-07-23 23:20:31 -0300

    Mascarando e modificando formulario

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
+const cpfInput = document.getElementById('cpf')
+
+function cpf(value) {
+    if (!value.match(/^(\d{3}).(\d{3}).(\d{3})-(\d{2})$/)) {
+        value = value.replace(/\D/g, "")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d{2})$/, "$1-$2")
+    }
+    return value
+}
+
+function cnpj(value) {
+    if (!value.match(/^(\d{2}).(\d{3}).(\d{3})\\(\d{4})-(\d{2})$/)) {
+        value = value.replace(/\D/g, "")
+        value = value.replace(/(\d{2})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1/$2")
+        value = value.replace(/(\d{4})(\d)/, "$1-$2")
+    }
+    return value
+}
+
+cpfInput.addEventListener('input', function (event) {
+    const clearnedInput = event.target.value.replace(/\D/g, '');
+    if (clearnedInput.length > 11) {
+        event.target.value = cnpj(clearnedInput)
+    } else {
+        event.target.value = cpf(clearnedInput)
+    }
+
+})
+
